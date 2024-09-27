fb69363ec5f83c08ddbcb8a1368d84720a2ef15a
Eric-Villagrasa <eric.villagrasa@campus.xaloc.org>
2024-09-27 10:20:56 +0200

    Add files via upload

---
+++ ejercicio_6.py
@@ -0,0 +1,10 @@
+# 6. A partir del programa 5. Haz que se muestre por pantalla también la frase en el orden inverso en que se han introducido las palabras
+
+var1=input("Introduce una palabra: ")
+var2=input("Introduce otra palabra: ")
+var3=input("Introduce otra palabra: ")
+var4=input("Introduce otra palabra: ")
+var5=input("Introduce otra palabra: ")
+
+print(var1,",",var2,",",var3,",",var4,",",var5)
+print(var5,",",var4,",",var3,",",var2,",",var1)
---
+++ ejercicio_9.py
@@ -0,0 +1,7 @@
+#9. programa que pida los segundos y muestre por pantalla y en la misma frase los minutos y las horas
+
+
+var1=int(input("Introduce el número de segundos: "))
+
+print("el número de minutos es: ",(var1/60))
+print("el número de horas: ",(var1/3600))
