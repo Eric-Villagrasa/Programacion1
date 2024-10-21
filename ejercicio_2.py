e1ece5585a5804808b81166533613f50fe8cb0ba
Eric-Villagrasa <eric.villagrasa@campus.xaloc.org>
2024-10-21 10:09:22 +0200

    Update ejercicio_2.py

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -1,4 +1,4 @@
-#2. Programa que introduzca por teclado tres tipos de variables y se muestren por pantalla en el siguiente orden: número entero, texto y número decimal.
+#2. Programa que introduzca porrr teclado tres tipos de variables y se muestren por pantalla en el siguiente orden: número entero, texto y número decimal.
 
 var1=int(input("Introduce variable de número: "))
 var2=input("Introduce variable de texto: ")
