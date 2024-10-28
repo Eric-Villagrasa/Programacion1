b1fbc95ac3387cfb57ce54b659d61fd90b687ccf
Eric-Villagrasa <eric.villagrasa@campus.xaloc.org>
2024-10-28 10:36:32 +0100

    Create ejercicio_33.py

---
+++ Ejercicios_condicionales/ejercicio_33.py
@@ -0,0 +1,12 @@
+#33. Programa un código que permita contar el número de vocales de la siguiente frase: No hay mal que dure cien años.
+
+var1="No hay mal que dure cien años"
+print(var1)
+
+a=var1.count("a")
+e=var1.count("e")
+i=var1.count("i")
+o=var1.count("o")
+u=var1.count("u")
+
+print(f"El número de a es {a}, el número de e es {e}, el número de i es {i}, el número de o es {o} y el número de u es {u}")
