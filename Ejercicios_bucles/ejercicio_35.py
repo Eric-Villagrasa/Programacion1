d70ca3e6486dc882a089da4cb2e5389d413c3a2a
Eric-Villagrasa <eric.villagrasa@campus.xaloc.org>
2024-11-15 10:12:37 +0100

    Create ejercicio_35.py

---
+++ Ejercicios_bucles/ejercicio_35.py
@@ -0,0 +1,7 @@
+#35. Programa que al introducir un número por teclado permita mostrar ese número de veces tu nombre.
+
+var1=int(input("Introduce un número: "))
+
+for var2 in range(var1):
+    print("Eric Villagrasa")
+  
